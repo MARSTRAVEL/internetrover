3b1efa064364c7e0b10aac4548f5a37e9dbba4cf
Rauha <yunlong.a.liu@gmail.com>
2019-07-30 17:36:43 +0300

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const se = require('./modules/contact.js');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const formidable = require('formidable');
-const mondbURL = "mongodb+srv://yunlong:longlovesuomi810@cluster0-m2asn.mongodb.net/internetrover?retryWrites=true&w=majority";
+// const mondbURL = "mongodb+srv://yunlong:longlovesuomi810@cluster0-m2asn.mongodb.net/internetrover?retryWrites=true&w=majority";
 const port = process.env.PORT || 8080;
 const localDB = 'mongodb://localhost:27017/internetRover';
 
@@ -18,12 +18,12 @@ app.use(session({
   cookie: { maxAge:1000*60*10 },
   // store current user in database and max age:...
   store: new MongoStore({
-    url: mondbURL || localDB ,
+    url: MONGOLAB_AMBER_URI || localDB ,
   }),
 }));
 // const MONGO_URI = "mongodb+srv://yunlong:<longlovesuomi810>@cluster0-m2asn.mongodb.net/test?retryWrites=true&w=majority";
 
-mongoose.connect(mondbURL || localDB, { useNewUrlParser: true});
+mongoose.connect(MONGOLAB_AMBER_URI || localDB, { useNewUrlParser: true});
 app.set('view engine', 'ejs');
 app.use(function(req, res, next){
 　　res.locals.user = req.session.user;
