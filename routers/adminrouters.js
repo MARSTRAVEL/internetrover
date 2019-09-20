99262f2ae05f4dc0a1eb70d342c76591916ab012
Rauha <yunlong.a.liu@gmail.com>
2019-09-20 16:52:33 +0300

    Remove /admin/Pages

--- routers/adminrouters.js
+++ routers/adminrouters.js
@@ -9,13 +9,4 @@ router.post('/login',admincontrol.postlogin);
 router.get('/jobhunting',admincontrol.jobhunting);
 router.post('/jobhunting',admincontrol.savejobhunting);
 
-router.post('/', admincontrol.postlogin);
-
-router.get('/pages', admincontrol.getpages);
-
-router.get('/index', function(req, res){
-  res.render('admin/index');
-});
-
-
 module.exports = router;
