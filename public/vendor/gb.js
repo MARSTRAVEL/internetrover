d4a3b25d47fe9970f1675faa2a4e77d68799aad2
Rauha <yunlong.a.liu@gmail.com>
2020-01-03 05:48:38 +0200

    cas

--- public/vendor/gb.js
+++ public/vendor/gb.js
@@ -6,5 +6,10 @@ $(document).on('ready', function() {
     slidesToShow: 4,
     slidesToScroll: 4
   });
+  const play = document.getElementById("playButton");
+  const video = document.getElementById('feateredVideos-video');
 
+  play.addEventListener("click", () => {
+     video.play();
+    }, false);
 });
